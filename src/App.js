c133cc6f450311312c07de41dbe8c8dcf321b341
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-06-20 08:09:10 +0530

    initial template

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  calculateMonths = (start, end) => {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <div className="text-center">
+          <h1>A Simple Work Experience Calculator!</h1>
+          <hr />
+        </div>
+
+        <div className="row">
+          
+          <input type="text" className="form-control col-md-3" placeholder="Comapnay Name" aria-label="Username" aria-describedby="basic-addon1" />
+          <input type="date" className="form-control col-md-3" placeholder="Start Month and Year" aria-label="Username" aria-describedby="basic-addon1" />
+          <input type="date" className="form-control col-md-3" placeholder="End Month and Year" aria-label="Username" aria-describedby="basic-addon1" />
+
+          <input type="text" className="form-control col-md-3" placeholder="Experience in Months" aria-label="Username" aria-describedby="basic-addon1" />
+          
+        </div>
+
       </div>
     );
   }
